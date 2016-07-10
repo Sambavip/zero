423716d6b205808bc5903a54bb8d4d784873507d
Sambavip <apsambavi95@gmail.com>
2016-07-10 19:38:41 +0530

    Add files via upload

---
+++ Ide.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    if(n>0)
+    printf(" no is positive");
+    else if(n<0)
+    printf("\n the number is negative");
+    else 
+    printf("the no is zero");
+
+}
